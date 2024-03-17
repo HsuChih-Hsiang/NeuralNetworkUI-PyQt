b9a2c4d48c8eed424ef53c64ff82c172d7d269db
Ming_Song Hsu <abab8018@gmail.com>
2024-03-18 00:28:13 +0800

    modify ui

--- manage_system/update_label_dialog.py
+++ manage_system/update_label_dialog.py
@@ -4,17 +4,20 @@ from PySide6.QtCore import *
 
 
 class UpdateLabelDialog(QDialog, update_label_dialog_ui.Ui_Dialog):
+    # name, is_show. description
     trans_label = Signal(str, bool, str)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.setupUi(self)
-        self.setWindowTitle('Label Dialog')
-        self.buttonBox.accepted.connect(self.return_name)
+        self.setWindowTitle('Update Label Dialog')
+        self.buttonBox.accepted.connect(self.return_args)
 
         self.show()
 
-    def return_name(self):
+    def return_args(self):
         label_name = self.label_name.text()
-        self.trans_label.emit(label_name)
+        is_show = self.is_show_checkBox.isChecked()
+        description = self.description_edit.toPlainText()
+        self.trans_label.emit(label_name, is_show, description)
         self.accept()
